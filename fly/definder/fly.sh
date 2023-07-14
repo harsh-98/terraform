b564240e39ab81d6a29f66e6c2349087aa73e0dc
harsh-98 <harshjniitr@gmail.com>
2023-07-14 19:14:57 +0530

    feat: expose healthcheck endpoint and allow fly current image deploy

--- fly/definder/fly.sh
+++ fly/definder/fly.sh
@@ -4,6 +4,10 @@ set -e
 
 FLY_APP=definder
 FILE_PATH=$(realpath `dirname $0`)
+
+IMAGE=`fly image show | ggrep  -oP "\K(deployment-\w+)"`
+IMAGE="registry.fly.io/"$FLY_APP":"$IMAGE
+
 cd $FILE_PATH/../../../$FLY_APP
 #
 VERSION=`go list  -m -u  github.com/Gearbox-protocol/go-liquidator  | awk '{print $2}'`
@@ -12,6 +16,11 @@ mkdir -p dist
 sudo rm -rf dist/go-liquidator
 cp -r "$GOPATH/pkg/mod/github.com/!gearbox-protocol/go-liquidator@$VERSION" ./dist/go-liquidator
 #
-fly deploy  --build-target app --dockerfile docker/Dockerfile.flyio  --config ../terraform/fly/$FLY_APP/fly.toml
+
+if [ "remote" = "$1" ]; then
+    fly deploy  --build-target app --dockerfile docker/Dockerfile.flyio  --config ../terraform/fly/$FLY_APP/fly.toml
+elif [ "image" =  "$1" ];then
+    fly deploy  --image $IMAGE  --config ../terraform/fly/$FLY_APP/fly.toml
+fi 
 cd $FILE_PATH
 
--- fly/go-liquidator/fly.sh
+++ fly/go-liquidator/fly.sh
@@ -4,9 +4,19 @@ set -e
 
 FLY_APP=go-liquidator
 FILE_PATH=$(realpath `dirname $0`)
+
+IMAGE=`fly image show | ggrep  -oP "\K(deployment-\w+)"`
+IMAGE="registry.fly.io/"$FLY_APP":"$IMAGE
+
 cd $FILE_PATH/../../../$FLY_APP
 #
-fly deploy  --build-target app --dockerfile docker/Dockerfile.flyio.$FLY_APP  --config ../terraform/fly/$FLY_APP/fly.toml
+
+
+#
+if [ "remote" = "$1" ]; then
+    fly deploy  --build-target app --dockerfile docker/Dockerfile.flyio.$FLY_APP  --config ../terraform/fly/$FLY_APP/fly.toml
+elif [ "image" =  "$1" ];then
+    fly deploy  --image $IMAGE  --config ../terraform/fly/$FLY_APP/fly.toml
+fi 
+
 cd $FILE_PATH
-
-
